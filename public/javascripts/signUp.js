665b07f846b1ab4116b14973b84ad81148293ee3
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-04-16 02:05:30 +0900

    sign upのform送信

--- public/javascripts/signUp.js
+++ public/javascripts/signUp.js
@@ -1,26 +1,28 @@
 $(function () {
     $('#signUp').click(
         function () {
-            const hostUrl = 'http://localhost:3000/registration';
-            const $email = $('#email');
-            const $password = $('#password');
-            const $name = $('#name');
-            const $studentNumber = $('#studentNumber');
+            alert("sign up clicked")
+            const hostUrl = './registration';
+
+            let jsonData = {
+                "email": $('#email').val(),
+                "password": $('#password').val(),
+                "name": $('#name').val(),
+                "studentNumber": $('#studentNumber').val()
+            }
+
             $.ajax({
-                url: hostUrl,
                 type: 'POST',
+                url: hostUrl,
                 dataType: 'json',
-                data: {
-                    email: $email,
-                    password: $password,
-                    name: $name,
-                    studentNumber: $studentNumber
-                },
-                timeout: 10000,
+                data: jsonData,
             }).done(function (data) {
-                alert("ok");
+                // alert(data)
+                // alert("ok");
+                window.location.href = './registration'; // 通常の遷移
             }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
-                alert("error");
-            })
+                alert("textStatus： " + textStatus);
+                alert("errorThrown： " + errorThrown);
+            });
         });
 });
--- routes/registration.js
+++ routes/registration.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log(req.query)
-    res.render('index', { title: 'registration' });
+    console.log("/registrationのGet")
+    res.redirect("https://www.yahoo.co.jp/")
 });
 
 router.post('/', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
     console.log(req.body)
-    res.render('index', { title: 'registration' });
+    console.log(res.statusCode)
+    res.json({ "status": "ok" })
 });
 
 module.exports = router;
