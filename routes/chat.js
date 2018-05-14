a58ccda0995f8cb37fc6af16ecebc67243555481
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-05-14 21:53:09 +0900

    test

--- routes/chat.js
+++ routes/chat.js
@@ -31,6 +31,10 @@ let slot = {
 
 const User = require('../models/user');
 
+const { google } = require('googleapis');
+const OAuth2Client = google.auth.OAuth2;
+var oAuth2Client = new OAuth2Client(null, null, null);
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -48,32 +52,38 @@ router.post('/webhook', function (req, res, next) {
     if (req.body.queryResult.intent.displayName == "名前") {
         User.find({ "name": req.body.queryResult.parameters.userName }, function (err, user) {
             userName = user[0].name;
-            userOauth = user[0].oauth;
+            oauth = user[0].oauth;
 
             console.log(userName);
-            console.log(userOauth);
+            console.log(oauth);
+
+
+            oAuth2Client.clientId_ = oauth.clientId_;
+            oAuth2Client.clientSecret_ = oauth.clientSecret_;
+            oAuth2Client.redirectUri_ = oauth.redirectUri_;
+            oAuth2Client.credentials = oauth.credentials;
+            // oauth2Client.transporter = DefaultTransporter {}; 本来はtransporterも格納しないといけないが，なしでもいけた
+            oAuth2Client.opts = oauth.opts;
+
+            console.log(oAuth2Client);
+
             res.json({ "fulfillmentText": userName });
         });
+
+
+        User.find({ "name": req.body.name }, function (err, user) {
+
+
+
+
+        });
+
+
+
     }
     else if (req.body.queryResult.intent.displayName == "予定確認") {
-        var options = {
-            url: '../api/check',
-            headers: {
-                "Content-type": "application/json",
-            },
-            json: {
-                "name": userName
-            }
-        };
-
-        request.post(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body.fulfillmentText);
-                response.json({ "fulfillmentText": body.fulfillmentText })
-            } else {
-                console.log('error: ' + response.statusCode);
-            }
-        });
+        listEvents(oAuth2Client);
+        res.json({ "fulfillmentText": "予約を承りました。" });
     }
     // else if (req.body.queryResult.intent.displayName == "予定追加") {
 
@@ -95,6 +105,38 @@ router.post('/webhook', function (req, res, next) {
     //         return false;
     //     }
     // }
+
+
+
+
+    function listEvents(auth, startDate, callback) {
+        var calendar = google.calendar('v3');
+        calendar.events.list({
+            auth: auth,
+            calendarId: 'primary',
+            timeMin: startDate + "T00:00:00+09:00",//timeMinとtimeMaxを設定することでその区間の予定のみを検索
+            timeMax: startDate + "T23:59:59+09:00",
+            maxResults: 10,//最大検索件数
+            singleEvents: true,
+            orderBy: 'startTime'//開始時間順に並べ替え
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
+            var events = response.items;
+            var eventList = [];
+            if (events.length == 0) {
+                callback(eventList);
+            } else {
+                for (var i = 0; i < events.length; i++) {
+                    var event = events[i];
+                    eventList.push(events[i]);
+                }
+                callback(eventList);
+            }
+        });
+    }
 });
 
 
