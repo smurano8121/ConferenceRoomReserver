ca028a7fb4b37ef22edc97af0af588728b8cff50
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-05-18 21:27:46 +0900

    test

--- routes/oauth.js
+++ routes/oauth.js
@@ -58,7 +58,7 @@ router.get('/', function (req, res, next) {
     function authorize(credentials, callback) {
         const { client_secret, client_id, redirect_uris } = credentials.web;
         let token = {};
-        const oAuth2Client = new google.auth.OAuth2(
+        oAuth2Client = new google.auth.OAuth2(
             client_id, client_secret, redirect_uris[0]);
 
         // Check if we have previously stored a token.
