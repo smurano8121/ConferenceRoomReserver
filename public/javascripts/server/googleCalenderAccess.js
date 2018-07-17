dac3e48de647fae36f7d31007dc39a4accb2d576
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-07-17 14:06:08 +0900

    test

--- public/javascripts/server/googleCalenderAccess.js
+++ public/javascripts/server/googleCalenderAccess.js
@@ -1,6 +1,6 @@
 const { google } = require('googleapis');
 const OAuth2Client = google.auth.OAuth2;
-const TOKEN_PATH = 'credentials.json';
+const TOKEN_PATH = '../../../credentials.json';
 
 exports.authorizeInsertEvents = function (credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.web;
