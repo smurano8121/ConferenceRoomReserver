cf129586c268e0cf5645b1c4369a91e073047977
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-08-06 18:04:08 +0900

    time-periodに変更

--- routes/chat.js
+++ routes/chat.js
@@ -50,8 +50,8 @@ router.post('/webhook', function (req, res, next) {
         console.log(req.body.queryResult.parameters);
 
         let date = req.body.queryResult.parameters.date.match(/\d{4}-\d{2}-\d{2}T/);    //「2018-07-18T17:00:00+09:00」の「2018-07-18T」部分の正規表現
-        let startTimeRegExr = req.body.queryResult.parameters.time[0].match(/\d{2}:\d{2}:\d{2}\W\d{2}:\d{2}/);  //「2018-07-18T17:00:00+09:00」の「17:00:00+09:00」部分の正規表現
-        let finishTimeRegExr = req.body.queryResult.parameters.time[1].match(/\d{2}:\d{2}:\d{2}\W\d{2}:\d{2}/); //「2018-07-18T17:00:00+09:00」の「17:00:00+09:00」部分の正規表現
+        let startTimeRegExr = req.body.queryResult.parameters.time-period.startTime.match(/\d{2}:\d{2}:\d{2}\W\d{2}:\d{2}/);  //「2018-07-18T17:00:00+09:00」の「17:00:00+09:00」部分の正規表現
+        let finishTimeRegExr = req.body.queryResult.parameters.time-period.endTime.match(/\d{2}:\d{2}:\d{2}\W\d{2}:\d{2}/); //「2018-07-18T17:00:00+09:00」の「17:00:00+09:00」部分の正規表現
 
         slot.startDateTime = date+startTimeRegExr;
         slot.finishDateTime = date+finishTimeRegExr;
