466bf1a3efbca3d3ba2eeedb60db2d68c13c3031
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-08-09 06:01:47 +0900

    parameters check

--- routes/chat.js
+++ routes/chat.js
@@ -48,6 +48,7 @@ router.post('/webhook', function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     if (req.body.queryResult.intent.displayName == "会議室予約") {
         console.log(req.body.queryResult.intent.displayName);
+        console.log(req.body.queryResult.parameters);
         if(!req.body.queryResult.allRequiredParamsPresent){
             res.json({ "fulfillmentText": req.body.queryResult.fulfillmentText });
         }else{
