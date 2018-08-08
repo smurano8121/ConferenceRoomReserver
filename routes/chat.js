888c0a0aa2019d3c39d8dac8f8f450e505e29848
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-08-08 15:21:44 +0900

    test

--- routes/chat.js
+++ routes/chat.js
@@ -128,11 +128,11 @@ router.post('/webhook', function (req, res, next) {
                 console.log(attendeesListFromDialogFlow.length)
                 var addData = { 'email' : attendeeMail };
                 attendees.push(addData) ;
-                if(counter == attendeesListFromDialogFlow.length){
-                    console.log(counter)
-                    res.json({ "fulfillmentText": "参加者は"+responseName+"ですね？合っていれば予約日時と場所を教えてください．間違っていればもう一度お願いします"});
-                }
             });
+            if(counter == attendeesListFromDialogFlow.length){
+                console.log(counter);
+                res.json({ "fulfillmentText": "参加者は"+responseName+"ですね？合っていれば予約日時と場所を教えてください．間違っていればもう一度お願いします"});
+            }
         });
         // console.log("参加者："+responseName);
         // res.json({ "fulfillmentText": "参加者は"+req.body.queryResult.queryText+"さんですね？合っていれば予約日時と場所を教えてください．間違っていればもう一度お願いします"});
